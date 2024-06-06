275858bef5559fbd6c9119825a2ca448ea2263f6
heonbin-kwon <147012710+heonbin-kwon@users.noreply.github.com>
2024-06-06 15:09:36 +0900

    add music

--- app/src/main/java/kr/ac/cu/moai/dcumusicplayer/PlayerActivity.java
+++ app/src/main/java/kr/ac/cu/moai/dcumusicplayer/PlayerActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,10 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,51 @@ public class PlayerActivity extends AppCompatActivity {
             TextView tvArtist = findViewById(R.id.tvArtist);
             String artist = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
             tvArtist.setText(artist);
+
+            SeekBar tvSeek = findViewById(R.id.tvSeek);
+
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(mp3file);
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(mp -> {
+                mp.start();
+                mp.setLooping(false);
+                tvSeek.setMax(mp.getDuration());
+                tvSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(fromUser){
+                            mediaPlayer.seekTo(progress);
+                        }
+                        if(seekBar.getMax() == progress){
+                            mediaPlayer.stop();
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        mediaPlayer.pause();
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        mediaPlayer.seekTo(seekBar.getProgress());
+                        mediaPlayer.start();
+                    }
+                });
+                thread = new Thread(() -> {
+                    while (mediaPlayer != null && mediaPlayer.isPlaying()){
+                        try {
+                            Thread.sleep(1000);
+                            runOnUiThread(() -> {
+                                int current = mediaPlayer.getCurrentPosition();
+                                tvSeek.setProgress(current);
+                            });
+                        }catch (Exception e){}
+                    }
+                });
+                thread.start();
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -49,6 +100,7 @@ public class PlayerActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         finish();
+        mediaPlayer.stop();
         return super.onSupportNavigateUp();
     }
 
